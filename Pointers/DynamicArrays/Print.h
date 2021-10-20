9f300b6f06a1d392ebf08670e4703a9cdec8720b
Ionina34 <igoninadasha0315@mail.ru>
2021-10-20 17:39:26 +0500

    SeparatedDone

---
+++ Pointers/DynamicArrays/Print.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<iostream>
+using namespace std;
+template<typename T>void Print(T arr[], const int n);
+template<typename T>void Print(T** arr, const int rows, const int cols);
+#include"Print.cpp"
